f487f172d62912b0e07137ce5daeb398c23f9609
purebluesong <mousecatchcat@yeah.net>
2015-04-23 21:35:09 +0800

    界面上的一些小操作，部分的解决了界面的问题

--- src/com/HITech/Simplife/MyActivity.java
+++ src/com/HITech/Simplife/MyActivity.java
@@ -1,12 +1,12 @@
 package com.HITech.Simplife;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.TextView;
 import com.HITech.DataOperate.DatabaseTest;
 import com.HITech.GlobalResource;
@@ -16,14 +16,12 @@ public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    private Button button1;
     private Button button2;
-    private Button tabButton1;
-    private Button tabButton2;
     private DatabaseTest databaseTest;
     private TextView mTextView;
     private TimeAx timeAx;
-//    private ADDActivity addActivity;
+    private TabHost tabHost;
+    private ADDActivity addActivity;
 
     private Mybuttonlistener buttonListener;
     private int CurrentX=0,CurrentY=0;//TextView左上角的像素位置
@@ -34,30 +32,19 @@ public class MyActivity extends Activity {
         initialScreen();
         timeAx = new TimeAx(this);
         buttonListener = new Mybuttonlistener();
-        databaseTest = new DatabaseTest(this);
-        mTextView.setText(databaseTest.Answer());
+        mTextView.setText("music music music music music music m ");
         regeisterTouchListener();
     }
     @Override
     public void onStart(){
         super.onStart();
-        mTextView.setText(databaseTest.AddAnswer());
     }
     class Mybuttonlistener implements OnClickListener {
         @Override
         public void onClick(View v) {
             switch (v.getId())
             {
-                case R.id.main_tab_button_1:
-                    break;
-                case R.id.main_button2:
-                    break;
-                case R.id.main_tab_button_2:
-                    Intent intent = new Intent();
-                    intent.setClass(getApplicationContext(), ADDActivity.class);
-                    startActivity(intent);
-                    finish();
-                    break;
+
             }
         }
 
@@ -66,14 +53,16 @@ public class MyActivity extends Activity {
     private void initialScreen(){
         GlobalResource.ScreenWidth=getScreenWidth();
         button2=(Button)findViewById(R.id.main_button2);
-        tabButton1=(Button)findViewById(R.id.main_tab_button_1);
-        tabButton2=(Button)findViewById(R.id.main_tab_button_2);
+        tabHost = (TabHost)findViewById(R.id.main_tabHost);
+        tabHost.setup();
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("tab1").setContent(R.id.main_tab_text1));
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("tab2").setContent(R.id.main_tab_text2));
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("tab3").setContent(R.id.main_tab_text3));
         mTextView = (TextView)findViewById(R.id.textView);
     }
 
     private void regeisterTouchListener(){
-        button2.setOnClickListener(buttonListener);
-        tabButton1.setOnClickListener(buttonListener);
+     //   button2.setOnClickListener(buttonListener);
     }
 
     public int getScreenWidth(){
